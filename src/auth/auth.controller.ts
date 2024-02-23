6071b08c15555198eed7489c95f49f7eb1c00d75
Evgeny <evgeny.efimenko@proton.me>
2024-02-23 17:43:51 +0500

    chore: validation for /login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,31 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { MagicLoginStrategy } from './magiclogin.stategy';
+import { PasswordlessLoginDto } from './dto/passwordless-login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
-    private strategy: MagicLoginStrategy
-  ) { }
+    private strategy: MagicLoginStrategy,
+  ) {}
 
   @Post('login')
-  login(@Req() req, @Res() res, @Body() body) {
-    // TODO: send magic link
-    this.authService.validateUser(body.destination)
-    return this.strategy.send(req, res)
+  login(
+    @Req() req,
+    @Res() res,
+    @Body(new ValidationPipe()) body: PasswordlessLoginDto,
+  ) {
+    this.authService.validateUser(body.destination);
+    return this.strategy.send(req, res);
   }
 
   @Get('login/callback')
